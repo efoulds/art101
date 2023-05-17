f7928ab119bfb4d9b128aa86baa9a75d7e9a1397
efoulds <eribaby@Gmail.com>
2023-05-16 18:47:01 -0700

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 10, Modifying the DOM
+// Author: Eri Foulds
+// Date: 5.16.23
 
 // Constants
 
 // Functions
+window.onload = function () {
+  document.getElementById("output");
+   var outputEl = document.getElementById("output");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  document.createElement("p");
+    var new1El = document.createElement("p");
+      new1El.innerHTML = "Now you see me";
+
+  document.createElement("j");
+   var new2El = document.createElement("j");
+      new2El.innerHTML = "Now you don't"
+
+  outputEl.appendChild(new1El);
+  outputEl.appendChild(new2El);
+
+  outputEl.style.color = "navy";
+
+
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
