40871309ae5c26c8a10c7682e19af7088bb35408
efoulds <eribaby@Gmail.com>
2023-05-22 20:49:10 -0700

    lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,32 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Constants
+
+// Functions
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+{
+  var myButton = document.getElementById("button");
+  myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p> The Sorting Hat has placed you into " + house + "</p>";
+    document.getElementById("output").innerHTML = newText;
+  })
+}
